e2b4de31c35a438ef47b01b66415ec63dfdcaffe
Mikaela Mazey <maz16001@byui.edu>
2021-03-02 15:21:11 -0700

    my bio in my branch has been created

--- example.py
+++ example.py
@@ -1,2 +1,2 @@
-print("Hello world")
-print("Jeremy Edited this line")
+print("This is Mikaela's branch") 
+print("My major is Data Science because I like statistics and programming. I am from Ontario, Canada but haven't been home in a year (corona).")
